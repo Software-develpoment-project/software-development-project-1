cfbba18e5c66da493c103d798c4d75f13c3a03e9
ayushghimiresharma95 <157111797+ayushghimiresharma95@users.noreply.github.com>
2025-04-02 12:59:40 +0300

    Initial Push

---
+++ src/main/java/codefusion/softwareproject1/Softwareproject1Application.java
@@ -0,0 +1,13 @@
+package codefusion.softwareproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Softwareproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Softwareproject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/codefusion/softwareproject1/Softwareproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package codefusion.softwareproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Softwareproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
