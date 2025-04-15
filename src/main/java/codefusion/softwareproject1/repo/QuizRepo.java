2d6597bae39637d6adcfd3ff9a10b7af4e13822f
Ahmad Jalal <syedmuhammadjalal99@gmail.com>
2025-04-15 12:36:16 +0300

    Revert "Merge branch 'Repository'"
    This reverts commit 7bc494d0e90e6ac463ebd0f30a0f3ce114843935, reversing
    changes made to dc8a07caaf37f9f9d72e3b5745d528af5698fae1.

--- src/main/java/codefusion/softwareproject1/repo/QuizRepo.java
+++ src/main/java/codefusion/softwareproject1/repo/QuizRepo.java
@@ -1,6 +1,7 @@
 package codefusion.softwareproject1.repo;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
 import codefusion.softwareproject1.Models.QuizClass;
 import java.util.List;
@@ -9,6 +10,10 @@ public interface QuizRepo extends JpaRepository<QuizClass, Long> {
     List<QuizClass> findByPublished(boolean published);
     List<QuizClass> findByTeacherId(Long teacherId);
     List<QuizClass> findByCategoriesId(int categoryId);
+=======
+public interface QuizRepo {
+
+>>>>>>> parent of 7bc494d (Merge branch 'Repository')
 =======
 public interface QuizRepo {
 
