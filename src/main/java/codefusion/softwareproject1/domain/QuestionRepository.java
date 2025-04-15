373eb207647eb19dcbb3849d681c08078a039386
Ahmad Jalal <syedmuhammadjalal99@gmail.com>
2025-04-15 12:26:17 +0300

    Refactor QuestionRepository and QuizRepository interfaces to remove JpaRepository inheritance and associated methods. This change simplifies the repository structure, potentially in preparation for a new implementation or to address design concerns. Ensure that the repositories are correctly defined for future use.

--- src/main/java/codefusion/softwareproject1/domain/QuestionRepository.java
+++ src/main/java/codefusion/softwareproject1/domain/QuestionRepository.java
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
- 
-=======
 package codefusion.softwareproject1.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-import codefusion.softwareproject1.Models.Questions;
+public interface QuestionRepository {
 
-@Repository
-public interface QuestionRepository extends JpaRepository<Questions, Long> {
-    List<Questions> findByQuizId(Long quizId);
-    Questions findById(long id);
 }
->>>>>>> 22c4c7a3c629be5557e84b87b9f8fad678d52f60
