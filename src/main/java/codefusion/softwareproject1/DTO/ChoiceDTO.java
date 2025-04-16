049d5410943c70f3e1da8aab89c11f71b3b876b6
Ahmad Jalal <syedmuhammadjalal99@gmail.com>
2025-04-16 14:09:03 +0300

    Just committing before pulling main

--- src/main/java/codefusion/softwareproject1/DTO/ChoiceDTO.java
+++ src/main/java/codefusion/softwareproject1/DTO/ChoiceDTO.java
@@ -8,14 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChoiceDTO {
-<<<<<<< HEAD
-    private Long id;
-    private String content;
-    private boolean isCorrect;
-    private Long questionId;
-=======
+
    
     private String questionText;
     private boolean answer;
->>>>>>> main
+
 }
